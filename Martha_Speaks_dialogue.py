d13b6540a508d51d70f59b90329d47a01bd547cb
lilyque <lrq2002@nyu.edu>
2024-03-31 15:20:01 -0400

    Added functional dialogue that is able to read script from website

---
+++ Martha_Speaks_dialogue.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://transcripts.foreverdreaming.org/viewtopic.php?t=88727&sid=adaf0e19cfcf4223206ee057b0ab7dba'
+
+# Send a GET request to the URL
+response = requests.get(url)
+
+# Check if the request was successful
+if response.status_code == 200:
+    # Parse the HTML content
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Find the <div> element containing the script
+    div_element = soup.find('div', class_='content')
+
+    # Check if the div element is found
+    if div_element:
+        # Extract the text of the div element
+        script_text = div_element.get_text(separator='\n').strip()
+        print(script_text)
+    else:
+        print("Div element not found on the page.")
+else:
+    print("Failed to retrieve the webpage.")
