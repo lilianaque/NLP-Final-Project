d2b7bc18a2320be0bc48add481a7edacfc390a02
jacksonngo <49739045+jacksonngo@users.noreply.github.com>
2024-04-02 09:52:42 -0400

    Update Martha_Speaks_dialogue.py

--- Martha_Speaks_dialogue.py
+++ Martha_Speaks_dialogue.py
@@ -1,10 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 url = 'https://transcripts.foreverdreaming.org/viewtopic.php?t=88727&sid=adaf0e19cfcf4223206ee057b0ab7dba'
 
 # Send a GET request to the URL
 response = requests.get(url)
+
+vocab_words = []
 
 # Check if the request was successful
 if response.status_code == 200:
@@ -19,6 +22,15 @@ if response.status_code == 200:
         # Extract the text of the div element
         script_text = div_element.get_text(separator='\n').strip()
         print(script_text)
+
+        # Use regular expression to match words that are in quotations and add to a vocab list
+        words = re.findall(r'"([^"]+)"', script_text)
+        vocab_words.extend(words)
+
+        # Use regular expression to find words that come after "a" or "an" and before "is"
+        words = re.findall(r'(?:\ba\b|\ban\b) (\w+) is', script_text, re.IGNORECASE)
+        vocab_words.extend(words)
+
     else:
         print("Div element not found on the page.")
 else:
